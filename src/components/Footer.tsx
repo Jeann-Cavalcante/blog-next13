4955ec7fb841ac260b99b051a5cc2be434e631bb
Jean <jeanncavalcante@gmail.com>
2023-05-09 15:27:59 -0300

    estrutura inicial

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <h3>
+        ©Jean-Cavalcante 2023
+      </h3>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Blog Tech</h1>
+      <div>
+        <form >
+          <input type="text" placeholder="Search" />
+        </form>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
