a601148698b36bebf9ec6fea8e964af9cd70839b
Jean <jeanncavalcante@gmail.com>
2023-05-11 21:38:34 -0300

    Carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import CardPosts from "@/components/CardPosts";
+import PopularPosts from "@/components/PopularPosts";
 
 export default function Home() {
   return (
     <main className="max-w-[800px] mx-auto pb-16 pt-14 px-1">
+      <PopularPosts />
       <CardPosts />
     </main>
   )
